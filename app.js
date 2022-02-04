92b05d3bf657cf25f488946ba8f525537242a438
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2022-02-04 07:27:06 +0200

    added frontend build folder

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ mongoose
 //getAllData(); currently disabled
 // use middlewares
 app.use(express.json());
+app.use(express.static("build"));
 app.use(cors());
 app.use("/players", playersRouter);
 app.use("/games", gamesRouter);
